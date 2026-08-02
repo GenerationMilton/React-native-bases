4f384119a08e64755adc3adbbcb2acbfa5d14f8e
Milton Munoz <livemilton@gmail.com>
2026-08-01 20:21:18 -0500

    feat(01-bases-react): :sparkles: bases to react native Functions

--- 01-bases/src/App.tsx
+++ 01-bases/src/App.tsx
@@ -1,14 +1,16 @@
 // import { BasicTypes } from "./typescript/BasicTypes";
+// import { ObjectLiterals } from "./typescript/ObjectLiterals";
 
 import "./App.css";
-import { ObjectLiterals } from "./typescript/ObjectLiterals";
+import { BasicFunctions } from "./typescript/BasicFunctions";
 
 function App() {
   return (
     <>
       <h1>React + TS</h1>
-      <ObjectLiterals />
+      {/* <ObjectLiterals /> */}
       {/*<BasicTypes /> */}
+      <BasicFunctions />
     </>
   );
 }
