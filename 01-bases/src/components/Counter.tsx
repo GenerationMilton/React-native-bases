b9087b2f4f76196204fa96a17c769f534907a296
Milton Munoz <livemilton@gmail.com>
2026-08-02 20:04:41 -0500

    feat(01-bases-react): :sparkles: bases to react native counter Component with custom hooks

--- 01-bases/src/components/Counter.tsx
+++ 01-bases/src/components/Counter.tsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
+import { UseCounter } from "../hooks/UseCounter";
 
 export const Counter = () => {
-  const [count, setCount] = useState<number>(10);
+  const { count, increaseBy } = UseCounter();
 
-  const increaseBy = (value: number) => {
-    //setCount(count + value);
-    //setCount((current) => current + value);
-    setCount(Math.max(value + count, 0));
-  };
   return (
     <>
       <h3 className="text-2xl">
